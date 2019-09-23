9839a32c4e472ff84256457882a7502730ae4e39
Reusk11 <eduardoalort@outlook.com>
2019-09-23 00:03:55 -0500

    Se agrega el 1.5 e 1.6

--- problema1_5.py
+++ problema1_5.py
@@ -1,1 +1,5 @@
 radio= int(input("Escriba el radio de un cilindro"))
+altu= int(input("Escriba la altura del cilindro"))
+vol= 3.1416 * (radio * radio) * altu
+area= 2 * 3.1416 * radio * altu
+print(f"El volumen de este cilindro es de: {vol} y su area es de: {area}")
---
+++ problema1_6.py
@@ -0,0 +1,3 @@
+dias= int(input("Introduzca el numero de dias"))
+seg= dias * 24 * 60 * 60
+print(f"En {dias} dias hay {seg} segundos") 
