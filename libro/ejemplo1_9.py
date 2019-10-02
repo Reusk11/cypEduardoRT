cffb85723669eb07b8591669df3f9fc29bf999ef
Reusk11 <eduardoalort@outlook.com>
2019-10-01 23:12:52 -0500

    Se agregaron los ejemplos 2.

--- libro/ejemplo1_9.py
+++ libro/ejemplo1_9.py
@@ -1,7 +1,12 @@
- NUM = int(input("dame un numero entero:"))
- CUA = NUM * NUM
- CUB = NUM ** 3
- print(f"el cuadrado de { NUM } es { CUA }")
- print(f"el cubo de { NUM } es { CUB } ")
-
-
+<<<<<<< HEAD
+num= int(input("Dame un número entero:"))
+cua= num * num
+cub= num ** 3
+print(f"el cuadrado de {num} es {cua}")
+=======
+num= int(input("dame un numero entero:"))
+cua = num * num
+cub = num ** 3
+print(f"el cuadrado de {num} es el {cua}")
+>>>>>>> 377f71077586869d219daba26bdd9fe8e0885199
+print(f"el cubo de {num} es {cub}")
